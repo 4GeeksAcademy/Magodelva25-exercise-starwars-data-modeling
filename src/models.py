560a240c801de783540c99d821a31c76663e4cb5
Mangodev <migudz@gmail.com>
2024-12-28 12:05:05 +0000

    skeleton complete

--- src/models.py
+++ src/models.py
@@ -7,23 +7,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Users_list (Base):
+    __tablename__='users-list'
+    user_id = Column(Integer, ForeignKey(''))
+    user_name = Column(String, ForeignKey(''))
+    user_active = Column(String, nulleable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User (Base):
+    __tablename__='user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(32), nulleable=False)
+    first_name = Column(String(32))
+    last_name = Column(String(32))
+    email = Column(String(32), nulleable=False)
+    password = Column(String, nulleable=False)
+    creation = Column(Integer)
+    fav_list = Column(String)
+    active = Column(String, nulleable=False)
+
+class Fav_list (Base):
+    __tablename__='fav-list'
+    user_id = Column(Integer)
+    item_id = Column(Integer)
+    item_name = Column(String)
+    item_type = Column(String)
+    item_description = Column(String)
+
+class Planets(Base):
+    __tablename__='planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    type = Column(String)
+    description = Column(String)
+
+class Characters(Base):
+    __tablename__='planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    type = Column(String)
+    description = Column(String)
+
+
+
 
     def to_dict(self):
         return {}
